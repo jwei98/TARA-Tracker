78ba707b4283f2c2e8d812425aeb345f1f56ca34
Justin Wei <justin.lj.wei@gmail.com>
2016-08-25 02:39:52 -0400

    Initial Commit

---
+++ TARA Tracker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TARA Tracker
+//
+//  Created by Justin Wei on 8/25/16.
+//  Copyright © 2016 Justin Wei. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
