f3c41041778da3b78e84d7a43f000a460479da7c
jwei98 <justin.lj.wei@gmail.com>
2016-09-17 16:54:56 -0400

    completed data structure

--- TARA Tracker/ViewController.swift
+++ TARA Tracker/ViewController.swift
@@ -14,6 +14,14 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     @IBOutlet weak var tableView: UITableView!
     var toDoItems = [ToDoItem]()
     let screenBound = UIScreen.main.bounds
+    let memory = UserDefaults.standard
+
+    var previousMonth : Int = 0
+    var previousDate : Int = 0
+    var currentMonth : Int = 0
+    var currentDate : Int = 0
+    
+    var minutesLog = [Int]()
     
     // hide status bar
     override var prefersStatusBarHidden : Bool {
@@ -23,26 +31,40 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // LOADING PAST DATA
+        // get last date logged and store in previousMonth & previousDate
+        previousMonth = memory.integer(forKey: "previousMonth")
+        previousDate = memory.integer(forKey: "previousDate")
+        // get stored minutesLog
+        if let temp = memory.array(forKey: "minutesLog") as? [Int] {
+            minutesLog = temp
+        }
+        
+        // get current date
+        let date = NSDate()
+        let calendar = NSCalendar.current
+        let components = calendar.dateComponents([.year, .month, .day, .hour, .minute], from: date as Date)
+        currentMonth = components.month!
+        currentDate = components.day!
+        
+        
+        // tableView setup
         tableView.dataSource = self
+        
         tableView.delegate = self
         tableView.register(TableViewCell.self, forCellReuseIdentifier: "cell")
         tableView.separatorStyle = .none
         tableView.backgroundColor = UIColor.gray
         tableView.alwaysBounceVertical = false
-        
-
         let screenHeight = screenBound.size.height
         tableView.rowHeight = screenHeight / 5
-        print(tableView.rowHeight)
         
-        if toDoItems.count > 0 {
-            return
-        }
         toDoItems.append(ToDoItem(text: "Yoga Based Movement"))
         toDoItems.append(ToDoItem(text: "Breathing"))
         toDoItems.append(ToDoItem(text: "Meditation"))
         toDoItems.append(ToDoItem(text: "Materials"))
         toDoItems.append(ToDoItem(text: "Submit Minutes"))
+
     }
     
     
@@ -114,7 +136,23 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
                 textField.keyboardType = UIKeyboardType.numberPad
             }
             alertController.addAction(UIAlertAction(title: "Cancel", style: UIAlertActionStyle.default,handler: nil))
-            alertController.addAction(UIAlertAction(title: "Submit", style: UIAlertActionStyle.default,handler: nil))
+            alertController.addAction(UIAlertAction(title: "Submit", style: UIAlertActionStyle.default,handler: { (_) in
+                if let temp = alertController.textFields?[0] {
+                    let textField = temp
+                    let userInputAsInt = Int(textField.text!)
+                    if (userInputAsInt != nil){
+                        self.logActivity(tName: taskName, time: userInputAsInt!)
+                    }
+                    else {
+                        print("Not an integer")
+                    }
+                }
+                else {
+                    print("Not an integer")
+                }
+                }
+            
+            ))
              self.present(alertController, animated: true, completion: nil)
         }
         // case for Materials
@@ -150,15 +188,69 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
        
     }
     
+    
+    
     // alert handlers
+    func logActivity(tName : String, time : Int) {
+        print(minutesLog)
+        
+        // handles a new log (including first ever log)
+        if (currentDate != previousDate || currentMonth != previousMonth || minutesLog == []) {
+            // add current date to log
+            minutesLog.append(self.currentMonth)
+            minutesLog.append(self.currentDate)
+            // set current date -> previous date
+            memory.set(currentMonth, forKey: "previousMonth")
+            memory.set(currentDate, forKey: "previousDate")
+            previousMonth = currentMonth
+            previousDate = currentDate
+            
+            // log minutes
+            if tName == "Yoga Based Movement" {
+                minutesLog.append(time)
+                minutesLog.append(0)
+                minutesLog.append(0)
+            }
+            else if tName == "Breathing" {
+                minutesLog.append(0)
+                minutesLog.append(time)
+                minutesLog.append(0)
+            }
+            else {
+                minutesLog.append(0)
+                minutesLog.append(0)
+                minutesLog.append(time)
+            }
+        }
+            
+        // if it's the same day as the last time the user logged...
+        else if (previousMonth == currentMonth && previousDate == currentDate) {
+            var subtractIndex = 0
+            if tName == "Yoga Based Movement" {
+                subtractIndex = 3
+            }
+            else if tName == "Breathing" {
+                subtractIndex = 2
+            }
+            else {
+                subtractIndex = 1
+            }
+            minutesLog[minutesLog.endIndex-subtractIndex] = minutesLog[minutesLog.endIndex-subtractIndex] + time
+        }
+        print(minutesLog)
+        memory.set(minutesLog, forKey: "minutesLog")
+        
+    }
+    
     let goToMaterials = { (action:UIAlertAction!) -> Void in
         UIApplication.shared.openURL(URL(string: "https://www.dropbox.com/sh/ovff9450hnfeypq/AAAHbLIJt11x_rfWyTcu-Ehxa?dl=0")!)
     }
     
-    
     func turnBackgroundColor(_ color: UIColor) {
         tableView.backgroundColor = color
     }
+    
+    
     
     // for performance/efficiency
     override var canBecomeFirstResponder : Bool {
@@ -167,5 +259,6 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     override var canResignFirstResponder : Bool {
         return true
     }
+    
 }
 
